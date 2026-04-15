41058c311b749bc71d66fe7805640f250eecd54d
Natalie Azcona <natalieazcona@MacBook-Pro-de-Natalie.local>
2026-04-15 20:14:38 +0200

    Add NavBarPublic with basic styles and routing

--- frontend/src/layout/public/NavBarPublic.jsx
+++ frontend/src/layout/public/NavBarPublic.jsx
@@ -1,14 +1,15 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom"
+import "./NavBarPublic.scss"
 
 const NavBarPublic = () => {
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/register">Register</NavLink>
-      <NavLink to="/login">Login</NavLink>
+    <nav className="navbar">
+      <NavLink to="/" className="navbar__logo">Wöw Class</NavLink>
+      <div className="navbar__links">
+        <NavLink to="/register">Register</NavLink>
+        <NavLink to="/login">Login</NavLink>
+      </div>
     </nav>
-      
-
   )
 }
 
--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -2,16 +2,19 @@ import { Route, Routes } from "react-router-dom"
 import Home from "../pages/Home"
 import Register from "../pages/Register"
 import Login from "../pages/Login"
+import NavBarPublic from "../layout/public/NavBarPublic"
 
 
 const AppRouter = () => {
   return (
-    <Routes>
+    <>
+      <NavBarPublic />
+      <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/register" element={<Register/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
-      
-    </Routes>
+      </Routes>
+    </>
   )
 }
 
