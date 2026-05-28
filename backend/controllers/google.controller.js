071bec064a1ffb4e3681de3d51ba0fa71f213ab8
Natalie Azcona <natalieazcona@MacBook-Pro-de-Natalie.local>
2026-05-28 19:30:54 +0200

    fix Google OAuth callback: use JWT state param instead of isAuth middleware

--- backend/controllers/google.controller.js
+++ backend/controllers/google.controller.js
@@ -1,16 +1,23 @@
 const oauth2Client = require('../config/google');
 const User = require('../models/User');
+const { verifyJwt } = require('../utils/token');
 
 //Readme - OAuth2 client - google api nodejs client
 
+
+//Define scope, genera url en offline para que google devuelva el reefresh_token, luego redirige al usuario a pantalla de permisos
 const googleAuth = (req, res) => {
+
+    const { token } = req.query
     const scopes = [
         'https://www.googleapis.com/auth/calendar'
       ];
       
       const url = oauth2Client.generateAuthUrl({
         access_type: 'offline',
-        scope: scopes
+        scope: scopes,
+        state: token,
+        prompt: 'consent'
       });
 
 return res.redirect(url)
@@ -19,12 +26,13 @@ return res.redirect(url)
 
 const googleCallback = async (req, res) => {
     try {
-        const {code} =  req.query
+        const {code, state } =  req.query
         const {tokens} = await oauth2Client.getToken(code)
         oauth2Client.setCredentials(tokens)
 
+        const verifiedState = verifyJwt(state)
 
-        const id = req.user._id
+        const id = verifiedState.id
         const {access_token, refresh_token } = tokens
         await User.findByIdAndUpdate(id,  {
             googleAccessToken: access_token,
